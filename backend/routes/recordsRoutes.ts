70ddea7c8d2bdcdbe9130d1f7e32822acbd4d2aa
Thristan Jay Nakila <thristanjaynakila@gmail.com>
2024-12-18 20:53:01 +0800

    added get totalCalories

--- backend/routes/recordsRoutes.ts
+++ backend/routes/recordsRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { getRecords, getRecordsByID } from "../controllers/recordsController";
+import { getRecords, getRecordsByID, totalCaloriesBurned } from "../controllers/recordsController";
 
 const recordsRoutes = Router();
 
 recordsRoutes.get("/getRecords", authMiddleware, getRecords);
 recordsRoutes.get("/getRecordsByID/:id", authMiddleware, getRecordsByID);
+recordsRoutes.get("/getTotalCaloriesBurned", authMiddleware, totalCaloriesBurned);
 
 export default recordsRoutes;
 
